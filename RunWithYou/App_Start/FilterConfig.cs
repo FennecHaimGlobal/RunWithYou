a3165ab40c315ea6a94c8bdc103bf7cb07c0e48d
Haimb <haimb@moblin.com>
2016-08-22 09:05:52 +0300

    Create new Project

---
+++ RunWithYou/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RunWithYou
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
+        }
+    }
+}
